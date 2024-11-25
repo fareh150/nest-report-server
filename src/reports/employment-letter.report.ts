37f4867502b920bfcf19da781aaa0828843429b5
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-25 17:32:42 +0100

    basic usage of styles

--- src/reports/employment-letter.report.ts
+++ src/reports/employment-letter.report.ts
@@ -1,8 +1,22 @@
-import { TDocumentDefinitions } from 'pdfmake/interfaces';
+import { StyleDictionary, TDocumentDefinitions } from 'pdfmake/interfaces';
+
+const style: StyleDictionary = {
+  header: {
+    fontSize: 18,
+    bold: true,
+    alignment: 'center',
+  },
+};
 
 export const getEmploymentLetterReport = (): TDocumentDefinitions => {
   const docDefinition: TDocumentDefinitions = {
-    content: ['CONSTANCIA DE EMPLEO'],
+    styles: style,
+    content: [
+      {
+        text: 'Employment Letter',
+        style: 'header',
+      },
+    ],
   };
   return docDefinition;
 };
