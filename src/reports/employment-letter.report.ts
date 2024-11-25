a197466f85a9ba9ad4e58d5c0044978b6b71214a
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-25 17:43:36 +0100

    more sections and styles

--- src/reports/employment-letter.report.ts
+++ src/reports/employment-letter.report.ts
@@ -5,6 +5,16 @@ const style: StyleDictionary = {
     fontSize: 18,
     bold: true,
     alignment: 'center',
+    margin: [0, 0, 0, 20], // [left, top, right, bottom]
+  },
+  body: {
+    alignment: 'justify',
+    margin: [0, 0, 0, 50], // [left, top, right, bottom]
+  },
+  signature: {
+    fontSize: 14,
+    bold: true,
+    alignment: 'center',
   },
 };
 
@@ -13,8 +23,31 @@ export const getEmploymentLetterReport = (): TDocumentDefinitions => {
     styles: style,
     content: [
       {
-        text: 'Employment Letter',
+        text: 'CONSTANCIA DE EMPLEO',
         style: 'header',
+      }, // Tambien te hacepta los enter para dejar espacio
+      {
+        text: `Yo, [Nombre del Empleador], en mi calidad de [Cargo del Empleador] de [Nombre de la Empresa],
+                por medio de la presente certifico que [Nombre del Empleado] ha sido empleado en nuestra
+                empresa desde el [Fecha de Inicio del Empleado].
+            
+
+                Durante su empleo, el Sr./Sra. [Nombre del Empleado] ha desempeñado el cargo de [Cargo del
+                Empleado], demostrando responsabilidad, compromiso y habilidades profesionales en sus
+                labores. \n\n
+                La jornada laboral del Sr./ Sra. [Nombre del Empleado] es de [Número de Horas] horas
+                semanales, con un horario de [Horario de Trabajo], cumpliendo con las políticas y
+                procedimientos establecidos por la empresa.\n\n
+                Esta constancia se expide a solicitud del interesado para los fines que considere conveniente.\n\n`,
+        style: 'body',
+      },
+      {
+        text: `Atentamente,
+                [Nombre del Empleador]
+                [Cargo del Empleador]
+                [Nombre de la Empresa]
+                [Fecha de Emisión]`,
+        style: 'signature',
       },
     ],
   };
