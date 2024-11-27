c63676f415c8852d86b8685d681f8eac35961578
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-27 08:10:43 +0100

    new start - in bash :  nest g resource storeReports --no-spec

---
+++ src/store-reports/store-reports.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { StoreReportsService } from './store-reports.service';
+
+@Controller('store-reports')
+export class StoreReportsController {
+  constructor(private readonly storeReportsService: StoreReportsService) {}
+}
