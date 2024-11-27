6c5059f05b895075c482c48227ad29bd75a92df4
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-27 13:01:44 +0100

    add logo - styles - first text

--- src/reports/order-by-id.report.ts
+++ src/reports/order-by-id.report.ts
@@ -1,7 +1,33 @@
-import { TDocumentDefinitions } from 'pdfmake/interfaces';
+import type {
+  Content,
+  TDocumentDefinitions,
+  StyleDictionary,
+} from 'pdfmake/interfaces';
+
+const logo: Content = {
+  image: 'src/assets/tucan-banner.png',
+  width: 100,
+  height: 30,
+  margin: [10, 30],
+};
+
+const styles: StyleDictionary = {
+  header: {
+    fontSize: 20,
+    bold: true,
+  },
+};
 
 export const orderByIdReport = (): TDocumentDefinitions => {
   return {
-    content: ['Reporte basico'],
+    styles,
+    header: logo,
+    pageMargins: [40, 70, 40, 60],
+    content: [
+      {
+        text: 'Tucan Code',
+        style: 'header',
+      },
+    ],
   };
 };
