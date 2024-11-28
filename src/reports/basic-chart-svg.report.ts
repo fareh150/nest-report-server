f9a09becd1497e3af4e1ba7ad465c8db61eb8140
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-28 13:30:20 +0100

    start of basic report

---
+++ src/reports/basic-chart-svg.report.ts
@@ -0,0 +1,8 @@
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+
+export const getBasicChartSvgReport =
+  async (): Promise<TDocumentDefinitions> => {
+    return {
+      content: [],
+    };
+  };
