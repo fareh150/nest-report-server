4bd49b11db1ba9aad57dc42e12c494f3f59cf96f
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-28 20:34:59 +0100

    improve chart util

--- src/helpers/chart-utils.ts
+++ src/helpers/chart-utils.ts
@@ -1,8 +1,26 @@
 import axios from 'axios';
 
-export const chartJsToImage = async (chartConfig: unknown) => {
+interface ChartOptions {
+  height?: number;
+  width?: number;
+}
+
+export const chartJsToImage = async (
+  chartConfig: unknown,
+  options: ChartOptions = {},
+) => {
+  const params = new URLSearchParams();
+
+  if (options.height) {
+    params.append('height', String(options.height));
+  }
+
+  if (options.width) {
+    params.append('width', String(options.width));
+  }
+
   const encoderUri = encodeURIComponent(JSON.stringify(chartConfig));
-  const chartUrl = `https://quickchart.io/chart?c=${encoderUri}`;
+  const chartUrl = `https://quickchart.io/chart?c=${encoderUri}&${params.toString()}`;
 
   const response = await axios.get(chartUrl, { responseType: 'arraybuffer' });
 
