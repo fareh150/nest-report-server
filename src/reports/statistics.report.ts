8fa64ecdcc05959b63263020a8bf0135cc7c4a48
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-11-30 10:06:29 +0100

    usage

--- src/reports/statistics.report.ts
+++ src/reports/statistics.report.ts
@@ -1,46 +1,5 @@
 import { TDocumentDefinitions } from 'pdfmake/interfaces';
-import * as Utils from '../helpers/chart-utils';
-import { title } from 'process';
-
-const generateTopCountryDonut = async (
-  topCountries: TopCountry[],
-): Promise<string> => {
-  const data = {
-    labels: topCountries.map((country) => country.country),
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: topCountries.map((country) => country.customers),
-        // backgroundColor: Object.values(Utils.CHART_COLORS),
-      },
-    ],
-  };
-
-  const config = {
-    type: 'doughnut',
-    data: data,
-    options: {
-      legend: {
-        position: 'left',
-      },
-      title: {
-        text: 'someTitle',
-        display: true,
-      },
-      plugins: {
-        datalabels: {
-          color: 'white',
-          font: {
-            weight: 'bold',
-            size: 16,
-          },
-        },
-      },
-    },
-  };
-
-  return Utils.chartJsToImage(config);
-};
+import { getDonutChart } from './charts';
 
 interface TopCountry {
   country: string;
@@ -56,7 +15,15 @@ interface ReportOptions {
 export const getStatisticsReport = async (
   options: ReportOptions,
 ): Promise<TDocumentDefinitions> => {
-  const donutChart = await generateTopCountryDonut(options.topCountries);
+  const donutChart = await getDonutChart({
+    entries: options.topCountries.map((country) => ({
+      label: country.country,
+      value: country.customers,
+    })),
+    showTitle: true,
+    titleText: 'Top 10 countries by customers',
+    position: 'bottom',
+  });
 
   const docDefinition: TDocumentDefinitions = {
     content: [
