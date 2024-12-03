f627452e7eff54677bc4e4d2ff6da6f015c8d63b
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-12-03 17:20:57 +0100

    basic configuration to show html in pdf

--- src/extra-reports/extra-reports.service.ts
+++ src/extra-reports/extra-reports.service.ts
@@ -1,12 +1,28 @@
+import fs from 'fs';
 import { Injectable } from '@nestjs/common';
 import { PrinterService } from 'src/printer/printer.service';
 import { getHelloWorldReport } from 'src/reports';
+import { getHtmlContent } from 'src/helpers';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+import { headerSection } from 'src/reports/sections/header.section';
 
 @Injectable()
 export class ExtraReportsService {
   constructor(private readonly printerService: PrinterService) {}
   getHtmlReport() {
-    const docDefinition = getHelloWorldReport({ name: 'Fareh Romero' });
+    // get the code of the html file
+    const html = fs.readFileSync('src/reports/html/basic-01.html', 'utf8');
+
+    const content = getHtmlContent(html);
+
+    const docDefinition: TDocumentDefinitions = {
+      pageMargins: [40, 110, 40, 60],
+      header: headerSection({
+        title: 'HTML Report',
+        subtitle: 'This is a simple HTML report',
+      }),
+      content,
+    };
 
     const doc = this.printerService.createPdf(docDefinition);
     return doc;
