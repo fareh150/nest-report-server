1472fe611db81c3f0cf205244b8f983b77f28ba2
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-12-03 17:54:14 +0100

    add image - some data - DEFAULT STYLEs

--- src/reports/community.report.ts
+++ src/reports/community.report.ts
@@ -1,10 +1,24 @@
 import type { TDocumentDefinitions } from 'pdfmake/interfaces';
 
-
-export const getCommunityReport = (
-): TDocumentDefinitions => {
+export const getCommunityReport = (): TDocumentDefinitions => {
   const docDefinition: TDocumentDefinitions = {
-    content: [`Hola community`],
+    defaultStyle: {
+      fontSize: 12,
+    },
+    content: [
+      {
+        columns: [
+          {
+            image: 'src/assets/tucan-code-logo.png',
+            width: 50,
+          },
+          {
+            alignment: 'center',
+            text: ' Forest Admin Community Report\nRUT: 123456789\nCamino montaña 123\nTelefono 123456789',
+          },
+        ],
+      },
+    ],
   };
 
   return docDefinition;
